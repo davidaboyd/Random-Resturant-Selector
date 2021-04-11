be965e488298cb36772c0452833ec48a3bd65ccc
davidaboyd <59895838+davidaboyd@users.noreply.github.com>
2021-04-11 15:59:59 -0400

    Add files via upload

---
+++ resturant selector.py
@@ -0,0 +1,13 @@
+import random
+
+resturant=[
+    '5 Guys',
+    'Zaxby\s',
+    'Mcdonald\s',
+    'Burger King',
+    'Panera Bread'
+]
+selected=random.choice(resturant)
+print('Where would you like to eat?')
+print(selected)
+    
